254c86d6bb220b1539f3a07de1a4751bea7c252c
otoofim <moh1371@gmail.com>
2019-08-29 17:08:58 +0430

    this code produce paired dataset to be used by CycleGAN

--- generating_of.py
+++ generating_of.py
@@ -4,15 +4,32 @@ import importlib
 
 from optical_flow import *
 
-addstr = '/media/mohammad/2CE86D4BE86D13FC/UCF-Anomaly-Detection-Dataset/UCF_Crimes/Videos/'
-files = os.listdir(addstr)
-Database = []
-for FILE in files:
-    if FILE in ['Normal_Videos_event', 'Training_Normal_Videos_Anomaly','Testing_Normal_Videos_Anomaly']:
-        new_path = addstr + FILE
-        files2 = os.listdir(new_path)
-        for FILE2 in files2:
-            if not (FILE2 in ['frames', 'opticalFlow']):
+
+def generator(addstr):
+    #addstr = '/media/mohammad/2CE86D4BE86D13FC/UCF-Anomaly-Detection-Dataset/UCF_Crimes/Videos/'
+    files = os.listdir(addstr)
+    Database = []
+    dest = ''
+    for FILE in files:
+        if FILE in ['Normal_Videos_event', 'Training_Normal_Videos_Anomaly','Testing_Normal_Videos_Anomaly']:
+            if FILE == "Training_Normal_Videos_Anomaly":
+                dest = "train"
+            elif FILE == "Testing_Normal_Videos_Anomaly":
+                dest = 'test'
+            else:
+                dest = "val"
+            new_path = addstr + FILE
+            files2 = os.listdir(new_path)
+            for FILE2 in files2:
+                #if not (FILE2 in ['fo']):
                 videopath = new_path + "/" + FILE2
-                optical_flow(videopath,1)
+                optical_flow(videopath, dest, 10)
                 #print(videopath)
+
+
+
+def main(add):
+   	optical_flow(add)
+
+if __name__ == "__main__":
+   	main(sys.argv[1])
--- optical_flow.py
+++ optical_flow.py
@@ -3,51 +3,74 @@ from subprocess import call
 import os
 import os.path
 import sys
+import numpy as np
 
-def optical_flow(video_dir, skip_frames):
+def optical_flow(video_dir, dest_add, skip_frames=10):
+
 
 	skip_frames = int(skip_frames)
 	video_name = video_dir.split('/')[-1].split('.')[0]
-	vidcap = cv2.VideoCapture(video_dir)
-	success,t1 = vidcap.read()
-	#t1 = cv2.resize(t1, (256,256), interpolation = cv2.INTER_AREA)
 
 
 	main_dir = os.path.abspath(os.path.join(video_dir, '..'))
+	#main_dir = os.path.abspath(os.path.join(main_dir, '..'))
+	main_dir = os.path.abspath(os.path.join(main_dir, '..'))
+	main_dir = main_dir + '/Normal_frame_optical'
 
-	if not (os.path.exists(main_dir+'/opticalFlow')):
-		os.mkdir(main_dir+'/opticalFlow')
-	if not (os.path.exists(main_dir+'/frames')):
-		os.mkdir(main_dir+'/frames')
+	print(main_dir)
 
-	count = 0
-	cv2.imwrite(main_dir+"/frames/t1_{}.ppm".format(count), t1)
+	if not (os.path.exists(main_dir)):
+		os.mkdir(main_dir)
+	if not (os.path.exists(main_dir+'/'+dest_add)):
+		os.mkdir(main_dir+'/'+dest_add)
+
+	main_dir = main_dir+'/'+dest_add
+
+
+
+	vidcap = cv2.VideoCapture(video_dir)
+	success,t1 = vidcap.read()
+	if success:
+		t1 = cv2.resize(t1, (256,256), interpolation = cv2.INTER_AREA)
+		count = 0
+		cv2.imwrite(main_dir+"/t1.ppm", t1)
+
 
 
 	while success:
+
 		success, t2 = vidcap.read()
-		count += 1
-		if (count%skip_frames == 0) and (not(os.path.exists("{}/opticalFlow/{}_{}_Flow.ppm".format(main_dir, video_name, count-skip_frames)))) and (success):
-			#t2 = cv2.resize(t2, (256,256), interpolation = cv2.INTER_AREA)
-			cv2.imwrite(main_dir+"/frames/t2_{}.ppm".format(count), t2)
-			print(main_dir+"/frames/t2_{}.ppm".format(count))
-			call('./optical/of {}/frames/t1_{} {}/frames/t2_{}'.format(main_dir, count-skip_frames, main_dir, count), shell=True)
-			os.rename("{}/frames/t1_{}.ppm".format(main_dir, count-skip_frames), "{}/frames/{}_{}.ppm".format(main_dir, video_name, count-skip_frames))
-			os.rename("{}/frames/t1_{}Flow.ppm".format(main_dir, count-skip_frames), "{}/opticalFlow/{}_{}_Flow.ppm".format(main_dir, video_name, count-skip_frames))
-			os.remove("{}/frames/t1_{}.flo".format(main_dir, count-skip_frames))
-			os.rename("{}/frames/t2_{}.ppm".format(main_dir, count), "{}/frames/{}_{}.ppm".format(main_dir, video_name, count))
-			cv2.imwrite("{}/frames/t1_{}.ppm".format(main_dir, count), t2)
-			#print(count)
-		elif (os.path.exists("{}/opticalFlow/{}_{}_Flow.ppm".format(main_dir, video_name, count-skip_frames))) and success:
-			os.remove("{}/frames/t1_{}.ppm".format(main_dir, count-skip_frames))
-			cv2.imwrite(main_dir+"/frames/t1_{}.ppm".format(count), t2)
 
-	os.remove("{}/frames/t1_{}.ppm".format(main_dir, count - (count%skip_frames) -1))
+		if success:
+
+			t2 = cv2.resize(t2, (256,256), interpolation = cv2.INTER_AREA)
+			cv2.imwrite(main_dir+"/t2.ppm", t2)
+			count += 1
+
+		if (count%skip_frames == 0) and (not(os.path.exists("{}/{}_{}.jpg".format(main_dir, video_name, count-skip_frames)))) and (success):
 
 
 
-# def main(add, skip_frames):
-# 	optical_flow(add, skip_frames)
+			call('./optical/of {}/t1 {}/t2'.format(main_dir, main_dir), shell=True)
+			im_AB = np.concatenate([cv2.imread('{}/t1.ppm'.format(main_dir), 1), cv2.imread("{}/t1Flow.ppm".format(main_dir), 1)], 1)
+			os.remove("{}/t1.flo".format(main_dir))
+			os.remove("{}/t1.ppm".format(main_dir))
+			os.rename("{}/t2.ppm".format(main_dir), "{}/t1.ppm".format(main_dir))
+			os.remove("{}/t1Flow.ppm".format(main_dir))
+
+			cv2.imwrite("{}/{}_{}.jpg".format(main_dir, video_name, count-skip_frames), im_AB)
+
+		elif (os.path.exists("{}/{}_{}.jpg".format(main_dir, video_name, count-skip_frames))) and success:
+			os.remove("{}/t1.ppm".format(main_dir))
+			os.rename("{}/t2.ppm".format(main_dir), "{}/t1.ppm".format(main_dir))
+
+
+	os.remove("{}/t1.ppm".format(main_dir))
+	os.remove("{}/t2.ppm".format(main_dir))
+
+
+# def main(add, dest_add, skip_frames):
+#   	optical_flow(add, dest_add, skip_frames)
 #
 # if __name__ == "__main__":
-# 	main(sys.argv[1], sys.argv[2])
+#   	main(sys.argv[1], sys.argv[2], sys.argv[3])
